87cb3511e5b818a069c6ba4aacc8532262c7790a
AgusBaudot <101721143+AgusBaudot@users.noreply.github.com>
2022-05-22 22:02:45 -0300

    Initial commit

---
+++ Assets/ObjectMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour {
+
+    public GameObject myCube;
+    public Vector3 movementVector;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (myCube.transform.position.x<13)
+        {
+            myCube.transform.position += movementVector;
+        }
+
+    }
+}
---
+++ Assets/ObjectPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPosition : MonoBehaviour {
+
+    public Vector3 newPosition;
+    public GameObject myCube;
+    
+	// Use this for initialization
+	void Start () {
+        myCube.transform.position = newPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
